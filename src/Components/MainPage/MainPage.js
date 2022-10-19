bf9a7a7eca20e09749e58e57edd7636cd62d65b4
Filipe Garrote <filipe.garrote@gmail.com>
2022-10-19 02:02:25 -0300

    feat: Submit box's layout implemented

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
 import TopMenu from '../../Common/TopMenu';
+import SubmitBox from './SubmitBox';
 
 export default function MainPage() {
 	return (
-		<>
+		<Wrapper>
 			<TopMenu />
-		</>
+			<SubmitBox />
+		</Wrapper>
 	);
 }
+
+const Wrapper = styled.main`
+	margin-top: 200px;
+	margin-left: 100px;
+`;
--- src/Components/MainPage/SubmitBox.js
+++ src/Components/MainPage/SubmitBox.js
@@ -1,11 +1,90 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 export default function SubmitBox() {
 	return (
-		<>
-			<img />
-			<a />
-		</>
+		<BoxStyle>
+			<img
+				src="https://sempreupdate.com.br/wp-content/uploads/2019/02/qual-a-diferenca-entre-programador-e-desenvolvedor.jpg"
+				alt="profile"
+			/>
+
+			<PostForm>
+				<h2>What are you going to share today?</h2>
+				<input type="text" name="link" placeholder="http://..."></input>
+				<textarea
+					type="text"
+					name="content"
+					placeholder="Awesome article about #javascript"
+				></textarea>
+				<button>Publish</button>
+			</PostForm>
+		</BoxStyle>
 	);
 }
+
+const BoxStyle = styled.div`
+	width: 611px;
+	height: 209px;
+	background-color: #ffffff;
+	border-radius: 16px;
+	padding: 18px;
+	display: flex;
+	justify-content: space-between;
+
+	img {
+		width: 50px;
+		height: 50px;
+		border-radius: 50px;
+		object-fit: cover;
+	}
+`;
+
+const PostForm = styled.form`
+	width: 88%;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+	gap: 5px;
+	margin-top: 8px;
+
+	h2 {
+		font-size: 20px;
+		font-weight: 300;
+		color: #707070;
+		width: 100%;
+		margin-bottom: 8px;
+	}
+
+	input,
+	textarea {
+		background-color: #efefef;
+		width: 100%;
+		height: 30px;
+		padding: 8px 13px;
+		border: none;
+		border-radius: 5px;
+		outline: none;
+
+		&::placeholder {
+			font-size: 15px;
+			font-weight: 300;
+			color: #949494;
+		}
+	}
+
+	textarea {
+		height: 66px;
+		resize: none;
+	}
+
+	button {
+		width: 112px;
+		height: 31px;
+		background-color: #1877f2;
+		border: none;
+		border-radius: 5px;
+		color: #ffffff;
+		font-size: 14px;
+		font-weight: 700;
+	}
+`;
